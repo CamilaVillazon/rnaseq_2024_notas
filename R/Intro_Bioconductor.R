7772b5647d29a40688e50c9f58998a1006d1ee7a
Camila Villazon <camilav@lcg.unam.mx>
2024-01-31 09:45:59 -0600

    Apunte Biocomputer

---
+++ R/Intro_Bioconductor.R
@@ -0,0 +1,23 @@
+#INTRODUCCIÓN A BIOCONDUCTOR
+
+#Paquetes:
+
+# 1. hapmapsnp6:
+# Es de uso interno. Pero la documentación es muy mala, no
+# informan que sistemas operativos se puede utilizar.
+
+
+# 2. iNETgrate
+# La documentación es descriptiva, incluye cómo descargarlo,
+# los sistemas operativos para los que funciona.
+# Hace tiempo que no se realizan preguntas sobre este paquete.
+# Pero es muy reciente, por lo que tal vez no es muy conocido.
+# Tiene un manual muy exhaustivo y actualizado.
+
+
+
+# 3. scider
+# Se comenzó a descargar más una vez que ya no fue tan nuevo,
+# pero han disminuido las descargas.Se puede descargar en diferentes
+# sistemas operativos, excepto Linux.
+#Tiene un manual extenso.
